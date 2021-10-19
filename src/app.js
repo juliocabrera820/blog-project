2e25a8366269ab14ebb6b74735a15721d15966f8
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-10-19 16:55:36 -0500

    style: apply format document

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-const routes = require("./routes")
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1", routes)
+app.use('/api/v1', routes);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const app = require('./app')
+const app = require('./app');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});

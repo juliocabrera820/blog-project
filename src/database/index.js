9dc61b9223458681ddf17255fc119d6030bbe17a
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-21 09:44:06 -0500

    set db connection and add user model

---
+++ src/database/index.js
@@ -0,0 +1,7 @@
+const sequelize = require('sequelize');
+const config = require('../config/database');
+const User = require('../app/models/user');
+
+const connection = new sequelize(config);
+User.init(connection);
+module.exports = connection;

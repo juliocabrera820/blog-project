6bf872e0af209f55de0915fceb4fd69181dad85c
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-23 13:03:14 -0500

    add method to get an access token

--- src/app/controllers/AuthenticationController.js
+++ src/app/controllers/AuthenticationController.js
@@ -1,4 +1,6 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const { JWT_SECRET, EXPIRATION_TIME } = require('../../config/jwt');
 
 class AuthenticationController {
   async signUp(req, res) {
@@ -12,6 +14,21 @@ class AuthenticationController {
     const user = await User.create({ username, email, password });
 
     return res.status(201).json(user);
+  }
+
+  async signIn(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ message: 'Email does not exist' });
+    }
+    if (password === user.password) {
+      const token = jwt.sign({ user }, JWT_SECRET, {
+        expiresIn: EXPIRATION_TIME,
+      });
+      return res.status(200).json(token);
+    }
+    return res.status(400).json({ message: 'Email or password incorrect' });
   }
 }
 

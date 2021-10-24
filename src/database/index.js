78c36a9d1c15f1ecce7af58b5f218542ff3063c9
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-24 13:20:41 -0500

    use new extension file

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const sequelize = require('sequelize');
-const config = require('../config/database');
+const config = require('../config/database.json');
 const User = require('../app/models/user');
 
 const connection = new sequelize(config);

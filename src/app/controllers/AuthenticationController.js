5b9b340e420608b2e0e7936ea540c862037340cf
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-24 16:01:00 -0500

    move code to try catch block

--- src/app/controllers/AuthenticationController.js
+++ src/app/controllers/AuthenticationController.js
@@ -4,31 +4,39 @@ const { JWT_SECRET, EXPIRATION_TIME } = require('../../config/jwt');
 
 class AuthenticationController {
   async signUp(req, res) {
-    const { username, email, password } = req.body;
-    const foundUser = await User.findOne({ where: { email } });
+    try {
+      const { username, email, password } = req.body;
+      const foundUser = await User.findOne({ where: { email } });
 
-    if (foundUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      if (foundUser) {
+        return res.status(400).json({ message: 'User already exists' });
+      }
+
+      const user = await User.create({ username, email, password });
+
+      return res.status(201).json(user);
+    } catch (error) {
+      return res.status(500).json({ message: 'server error', error });
     }
-
-    const user = await User.create({ username, email, password });
-
-    return res.status(201).json(user);
   }
 
   async signIn(req, res) {
-    const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return res.status(404).json({ message: 'Email does not exist' });
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return res.status(404).json({ message: 'Email does not exist' });
+      }
+      if (password === user.password) {
+        const token = jwt.sign({ user }, JWT_SECRET, {
+          expiresIn: EXPIRATION_TIME,
+        });
+        return res.status(200).json(token);
+      }
+      return res.status(400).json({ message: 'Email or password incorrect' });
+    } catch (error) {
+      return res.status(500).json({ message: 'server error', error });
     }
-    if (password === user.password) {
-      const token = jwt.sign({ user }, JWT_SECRET, {
-        expiresIn: EXPIRATION_TIME,
-      });
-      return res.status(200).json(token);
-    }
-    return res.status(400).json({ message: 'Email or password incorrect' });
   }
 }
 

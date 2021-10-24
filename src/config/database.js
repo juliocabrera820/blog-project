382212f443eccfb7f61fbaa9a7ded4dff73bff54
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-23 23:26:11 -0500

    connect to db

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_development`,
-  },
-  test: {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_test`,
-  },
-  production: {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'root',
-    password: null,
-    database: 'database',
-  },
+    development: {
+        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: `${process.env.DB_NAME}_development`,
+    },
 };
--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,18 @@ const sequelize = require('sequelize');
 const config = require('../config/database');
 const User = require('../app/models/user');
 
-const connection = new sequelize(config);
+let connection = {}
+
+if (process.env.NODE_ENV === 'production') {
+    connection = new sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        port: 5432,
+        host: '<heroku host>'
+    })
+} else {
+    connection = new sequelize(config);
+}
+
 User.init(connection);
 module.exports = connection;

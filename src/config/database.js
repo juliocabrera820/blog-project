e6d78114b8bf1ee85923bfe69d2a6e1f99113a65
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-23 22:34:46 -0500

    move config to development environment

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  development: {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
 };

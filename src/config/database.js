115c3d751dc0e6309d16b64e149a295634b9af9d
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-23 23:16:45 -0500

    add environments

--- src/config/database.js
+++ src/config/database.js
@@ -6,10 +6,20 @@ module.exports = {
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: `${process.env.DB_NAME}_development`,
+  },
+  test: {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_test`,
   },
   production: {
-    DATABASE_URL: process.env.DATABASE_URL,
     dialect: 'postgres',
+    host: 'localhost',
+    username: 'root',
+    password: null,
+    database: 'database',
   },
 };

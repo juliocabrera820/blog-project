4b6dafd6414eec71e2efcaad35199c3741785cb6
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-23 23:38:43 -0500

    connect to db

--- src/config/database.js
+++ src/config/database.js
@@ -8,4 +8,11 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: `${process.env.DB_NAME}_development`,
     },
+    production: {
+        dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    }
 };
--- src/database/index.js
+++ src/database/index.js
@@ -2,18 +2,7 @@ const sequelize = require('sequelize');
 const config = require('../config/database');
 const User = require('../app/models/user');
 
-let connection = {}
-
-if (process.env.NODE_ENV === 'production') {
-    connection = new sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        port: 5432,
-        host: '<heroku host>'
-    })
-} else {
-    connection = new sequelize(config);
-}
+const connection = new sequelize(config);
 
 User.init(connection);
 module.exports = connection;

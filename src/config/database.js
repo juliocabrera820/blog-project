1e214d84582503c3dce38032e6750f8cd7d607a3
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-24 15:53:42 -0500

    add development config

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    database: 'blog_development',
-    dialect: 'postgres',
-  },
-  test: {
-    database: 'blog_test',
-    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
   },
   production: {
     use_env_variable: 'DATABASE_URL',

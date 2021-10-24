1543bacdc7a39a7d94fd7261ddf146a073948125
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-23 23:53:30 -0500

    connect to production db

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
-        dialect: process.env.DB_DIALECT,
-        host: process.env.DB_HOST,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: `${process.env.DB_NAME}_development`,
-    },
-    production: {
-        dialect: process.env.DB_DIALECT,
-        host: process.env.DB_HOST,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    }
+  development: {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+  },
 };

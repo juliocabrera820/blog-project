58edabf5b26990520dc73b3f1171a1b2564024bd
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-28 23:06:59 -0500

    add validator

---
+++ src/app/validators/signInValidator.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+class SignInValidator {
+  check(req, res, next) {
+    const schema = Joi.object({
+      email: Joi.string().trim().required(),
+      password: Joi.string().trim().required(),
+    });
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ message: 'There were errors', error });
+    }
+    next();
+  }
+}
+
+module.exports = new SignInValidator();
---
+++ src/app/validators/signUpValidator.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+class SignUpValidator {
+  check(req, res, next) {
+    const schema = Joi.object({
+      username: Joi.string().trim().required(),
+      email: Joi.string().trim().required(),
+      password: Joi.string().trim().required(),
+    });
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ message: 'There were errors', error });
+    }
+    next();
+  }
+}
+
+module.exports = new SignUpValidator();

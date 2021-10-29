df8a770ec610d9071a2f4a5c6f5dc3b0ca2c94a0
juliocabrera820 <juliocabrera820@gmail.com>
2021-10-28 23:08:20 -0500

    use validator as middleware

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const authenticationController = require('./app/controllers/AuthenticationController');
 const suscriptionController = require('./app/controllers/SuscriptionController');
+const signInValidator = require('./app/validators/signInValidator');
+const signUpValidator = require('./app/validators/signUpValidator');
 
-router.post('/signUp', authenticationController.signUp);
-router.post('/signIn', authenticationController.signIn);
+router.post('/signUp', signUpValidator.check, authenticationController.signUp);
+router.post('/signIn', signInValidator.check, authenticationController.signIn);
 router.post('/suscription', suscriptionController.sendEmail);
 
 module.exports = router;

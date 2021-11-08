44c35b8f81f3a0592b6c2d565804a57a1911740c
juliocabrera820 <juliocabrera820@gmail.com>
2021-11-08 09:44:37 -0600

    add validations and relationship

---
+++ src/app/models/comment.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Comment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        userId: {
+          type: DataTypes.INTEGER,
+          validate: {
+            notEmpty: { msg: 'userId is missing' },
+          },
+        },
+        movieId: {
+          type: DataTypes.INTEGER,
+          validate: {
+            notEmpty: { msg: 'movieId is missing' },
+          },
+        },
+        content: {
+          type: DataTypes.STRING,
+          validate: {
+            notEmpty: { msg: 'content is missing' },
+            len: [5, 200],
+          },
+        },
+      },
+      { sequelize, tableName: 'Comments' }
+    );
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+  }
+}
+
+module.exports = Comment;

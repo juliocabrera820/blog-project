f9597e4cc3ef21fb3358869f072f32648acd7cfe
juliocabrera820 <juliocabrera820@gmail.com>
2021-11-24 23:58:54 -0600

    feat: send newsletter

--- src/app/controllers/SuscriptionController.js
+++ src/app/controllers/SuscriptionController.js
@@ -3,8 +3,28 @@ const nodemailer = require('nodemailer');
 const nodemailerSendgrid = require('nodemailer-sendgrid');
 const { SENDGRID_API_KEY, PROVIDER } = require('../../config/sendgrid');
 
+const PDFPrinter = require('pdfmake');
+const fs = require('fs');
+const fonts = require('../../utils/pdf/fonts');
+const styles = require('../../utils/pdf/styles');
+const { content } = require('../../utils/pdf/content');
+const path = require('path');
+
 class SuscriptionController {
   async sendEmail(req, res) {
+    let attachment = {
+      content,
+      styles,
+    };
+
+    const printer = new PDFPrinter(fonts);
+
+    let PDFDocument = printer.createPdfKitDocument(attachment);
+    PDFDocument.pipe(
+      fs.createWriteStream(path.resolve('src', 'pdfs', 'newsletter.pdf'))
+    );
+    PDFDocument.end();
+
     const transporter = nodemailer.createTransport(
       nodemailerSendgrid({ apiKey: SENDGRID_API_KEY })
     );
@@ -15,6 +35,12 @@ class SuscriptionController {
         to: email,
         subject: 'hello world',
         html: '<h1>Hello world!</h1>',
+        attachments: [
+          {
+            filename: 'newsletter.pdf',
+            path: path.resolve('src', 'pdfs', 'newsletter.pdf'),
+          },
+        ],
       });
       return res.status(200).json({ message: 'Email was successfully sent' });
     } catch (error) {

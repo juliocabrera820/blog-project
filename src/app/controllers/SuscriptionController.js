914de74cf3fb79a0b94ed03dba1033a95b0fb407
juliocabrera820 <juliocabrera820@gmail.com>
2021-12-01 19:09:24 -0600

    export movies to excel

--- src/app/controllers/SuscriptionController.js
+++ src/app/controllers/SuscriptionController.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
 const User = require('../models/user');
 const Newsletter = require('../../utils/newsletter');
+const TMDBService = require('../services/tmdb');
+const excel = require('../../utils/excel');
+const path = require('path')
 
 class SuscriptionController {
   async sendNewsletter(req, res) {
     const users = await User.findAll();
+    const movies = TMDBService()
+    excel(movies, 'movies', path.resolve('src', 'excelSheets', 'movies.xlsx'))
     users.forEach((user) => Newsletter.create(user.username));
     users.forEach((user) => {
       Newsletter.sendEmail(user);

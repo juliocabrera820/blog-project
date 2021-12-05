b6034fc16a9db67116a2be92a83266db39708e76
juliocabrera820 <juliocabrera820@gmail.com>
2021-12-05 14:33:28 -0600

    test: retrieve comments

--- tests/comment.spec.js
+++ tests/comment.spec.js
@@ -2,8 +2,9 @@
 const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/app/models/user');
+const Comment = require('../src/app/models/comment');
 
-describe('POST /users/:userId/comments', () => {
+describe('POST /users/comments', () => {
   beforeEach(async () => {
     await User.sync({ force: true });
     await request(app)
@@ -89,5 +90,152 @@ describe('POST /users/:userId/comments', () => {
       .set('Authorization', `Bearer ${token}s`);
     expect(status).toBe(401);
     expect(body.message).toBe('Token can not be decoded');
+  });
+});
+
+describe('GET /users/comments', () => {
+  beforeEach(async () => {
+    await User.sync({ force: true });
+    await Comment.sync({force: true})
+    await request(app)
+      .post('/api/v1/signUp')
+      .send({ username: 'jules', email: 'jules@gmail.com', password: '12345' });
+  });
+
+  test('should return 200 status code and an empty array', async () => {
+    const { body: token } = await request(app)
+      .post('/api/v1/signIn')
+      .send({ email: 'jules@gmail.com', password: '12345' });
+    const { status, body } = await request(app)
+      .get('/api/v1/users/comments')
+      .set('Authorization', `Bearer ${token}`);
+    expect(status).toBe(200);
+    expect(body).toEqual([])
+  });
+  test('should return 200 status code and comments', async () => {
+    const { body: token } = await request(app)
+      .post('/api/v1/signIn')
+      .send({ email: 'jules@gmail.com', password: '12345' });
+    await request(app)
+      .post('/api/v1/users/comments')
+      .send({ title: 'chucky', movieId: '1', content: 'good guy' })
+      .set('Authorization', `Bearer ${token}`);
+    const { status, body } = await request(app)
+      .get('/api/v1/users/comments')
+      .set('Authorization', `Bearer ${token}`);
+    expect(status).toBe(200);
+    expect(body.length).toBe(1)
+  });
+});
+
+describe('GET /users/comments/:commentId', () => {
+  beforeEach(async () => {
+    await User.sync({ force: true });
+    await Comment.sync({force: true})
+    await request(app)
+      .post('/api/v1/signUp')
+      .send({ username: 'jules', email: 'jules@gmail.com', password: '12345' });
+  });
+
+  test('should return 200 status code and comments', async () => {
+    const { body: token } = await request(app)
+      .post('/api/v1/signIn')
+      .send({ email: 'jules@gmail.com', password: '12345' });
+    await request(app)
+      .post('/api/v1/users/comments')
+      .send({ title: 'chucky', movieId: '1', content: 'good guy' })
+      .set('Authorization', `Bearer ${token}`);
+    const{ body, status } = await request(app)
+      .get('/api/v1/users/comments/1')
+      .set('Authorization', `Bearer ${token}`);
+    expect(status).toBe(200);
+    expect(body.id).toBe(1);
+    expect(body.userId).toBe(1);
+    expect(body.movieId).toBe(1);
+    expect(body.content).toBe('good guy');
+    expect(body.title).toBe('chucky');
+  });
+});
+
+describe('PUT /users/comments/:commentId', () => {
+  beforeEach(async () => {
+    await User.sync({ force: true });
+    await Comment.sync({force: true})
+    await request(app)
+      .post('/api/v1/signUp')
+      .send({ username: 'jules', email: 'jules@gmail.com', password: '12345' });
+  });
+
+  test('should return 200 status code and comments', async () => {
+    const { body: token } = await request(app)
+      .post('/api/v1/signIn')
+      .send({ email: 'jules@gmail.com', password: '12345' });
+    await request(app)
+      .post('/api/v1/users/comments')
+      .send({ title: 'chucky', movieId: '1', content: 'good guy' })
+      .set('Authorization', `Bearer ${token}`);
+    const{ status } = await request(app)
+      .put('/api/v1/users/comments/1')
+      .send({ content: 'bad guy' })
+      .set('Authorization', `Bearer ${token}`);
+    expect(status).toBe(200);
+  });
+});
+
+describe('DELETE /users/comments/:commentId', () => {
+  beforeEach(async () => {
+    await User.sync({ force: true });
+    await Comment.sync({force: true})
+    await request(app)
+      .post('/api/v1/signUp')
+      .send({ username: 'jules', email: 'jules@gmail.com', password: '12345' });
+  });
+
+  test('should return 200 status code and comments', async () => {
+    const { body: token } = await request(app)
+      .post('/api/v1/signIn')
+      .send({ email: 'jules@gmail.com', password: '12345' });
+    await request(app)
+      .post('/api/v1/users/comments')
+      .send({ title: 'chucky', movieId: '1', content: 'good guy' })
+      .set('Authorization', `Bearer ${token}`);
+    const{ body, status } = await request(app)
+      .delete('/api/v1/users/comments/1')
+      .set('Authorization', `Bearer ${token}`);
+    expect(status).toBe(200);
+  });
+});
+
+describe('GET /movies/:movieId/comments', () => {
+  beforeEach(async () => {
+    await User.sync({ force: true });
+    await Comment.sync({force: true})
+    await request(app)
+      .post('/api/v1/signUp')
+      .send({ username: 'jules', email: 'jules@gmail.com', password: '12345' });
+    await request(app)
+      .post('/api/v1/signUp')
+      .send({ username: 'yuls', email: 'yuls@gmail.com', password: '12345' });
+  });
+
+  test('should return 200 status code and comments', async () => {
+    const { body: tokenJules } = await request(app)
+      .post('/api/v1/signIn')
+      .send({ email: 'jules@gmail.com', password: '12345' });
+    await request(app)
+      .post('/api/v1/users/comments')
+      .send({ title: 'chucky', movieId: '1', content: 'good guy' })
+      .set('Authorization', `Bearer ${tokenJules}`);
+    const { body: tokenYuls } = await request(app)
+      .post('/api/v1/signIn')
+      .send({ email: 'yuls@gmail.com', password: '12345' });
+    await request(app)
+      .post('/api/v1/users/comments')
+      .send({ title: 'chucky', movieId: '1', content: 'funny guy' })
+      .set('Authorization', `Bearer ${tokenYuls}`);
+    const{ body, status } = await request(app)
+      .get('/api/v1/movies/1/comments')
+    expect(status).toBe(200);
+    expect(body.length).toBe(2);
   });
 });

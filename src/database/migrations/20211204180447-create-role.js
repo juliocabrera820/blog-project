7affbc4c49e1f0d48e84d1cdde8dcb8efa3b2162
juliocabrera820 <juliocabrera820@gmail.com>
2021-12-04 19:23:23 -0600

    add role column

---
+++ src/database/migrations/20211204180447-create-role.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Users', 'role', { type: Sequelize.STRING} )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Users', 'role')
+  }
+};
